61db04c33cab63f48285c44975e4a9083ee3585b
Ekaterina Topchieva <ekaterinatop4ieva@jandex.ru>
2023-01-18 22:10:36 +0300

    "feat: add Header"

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './/Header.css';
+
+function Header() {
+    return (
+        <header className="header">
+          <p className="header__text">Ты сегодня покормил кота?</p>
+        </header>
+    )
+}
+
+export default Header;
